59224b9f976d67ee7765db014fd8c9067ed33d5c
Thomas Devroye <thomasdevroye@MBPdeThsDevroye.lan>
2021-05-07 15:10:01 +0200

    test

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id= "app"> <H1> todolist </H1>
+  </div>
+</template>
+
+<script>
+import todolist from './ToDoList'
+export default {
+    name:'app',
+    components: {Todolist},
+    data () { 
+       return {
+           msg: 'welcome'
+        }
+    }
+}
+</script>
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+   <H1> Todolist </H1>
+   <Todolist> </Todolist>
+  </div>
+</template>
+
+<script>
+import Todolist from './ToDoList.vue'
+export default {
+    name:'app',
+    components: {Todolist},
+    data () { 
+       return {
+           msg: 'welcome'
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+/*li {
+  display: inline-block;
+  margin: 0 10px;
+}*/
+
+a {
+  color: #42b983;
+}
+</style>
